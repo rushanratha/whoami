458ffa6ac9db0e6d065f144bc2884c3e2ae25278
Rushan Ratha <28103376+echo-corp@users.noreply.github.com>
2022-12-12 14:42:10 +0000

    Add custom 404 catcher.

--- whoami-service/src/main.rs
+++ whoami-service/src/main.rs
@@ -1,11 +1,10 @@
 #[macro_use] extern crate rocket;
 
+use rocket::{serde::json::{Json, Value, serde_json::json}, Request};
+use whoami_service::AppStatus;
 use user::User;
 
 pub mod user;
-
-use rocket::serde::json::{Json, Value, serde_json::json};
-use whoami_service::AppStatus;
 
 #[get("/")]
 fn index() -> Json<AppStatus> {
@@ -18,9 +17,16 @@ fn post_user(user: Json<User>) -> Value {
     json!({"status" : "ok"})
 }
 
+#[catch(404)]
+fn not_found(req: &Request) -> Value {
+    json!({"message" : "The requested resource could not be found."})
+}
+
 #[launch]
 fn start() -> _ {
     rocket::build()
         .mount("/", routes![index])
         .mount("/", routes![post_user])
+        .register("/", catchers![not_found])
+
 }
